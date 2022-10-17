1b32d5a8798f14a265b095e26192f0841093f8c4
Daniel-Ope06 <opeoluwaoyedeji06@gmail.com>
2022-10-17 11:19:21 +0100

    add popup to delete button

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Database } from '@angular/fire/database';
 import { Food } from '../../models/food.model';
 import { FoodDB } from '../../services/foodDB.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
 
 @Component({
   selector: 'app-table',
@@ -11,7 +13,7 @@ import { FoodDB } from '../../services/foodDB.service';
 export class TableComponent implements OnInit {
   tableData: Food[];
 
-  constructor(public database: Database) { 
+  constructor(public database: Database, public dialog: MatDialog) { 
     this.tableData = FoodDB.getAllFoods(this.database);
   }
 
@@ -28,5 +30,9 @@ export class TableComponent implements OnInit {
 
     // remove from firebase
     FoodDB.deleteFromFoodDB(this.database, foodName);
+  }
+
+  openDeleteDialog(foodName: string, foodCategory: string, foodCost: number): void{
+    this.dialog.open(DeleteDialogComponent);
   }
 }
